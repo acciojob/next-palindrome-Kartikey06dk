f2a8ffd1aa00eef44ca516f67c93149725a3e1bd
Kartikey Singh <92157475+Kartikey06dk@users.noreply.github.com>
2023-09-07 14:34:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 function nextPalindrome(num) {
-  //your JS code here.
+  while (true) {
+    num++;
+    if (isPalindrome(num)) {
+      return num;
+    }
+  }
+}
+
+function isPalindrome(number) {
+  const str = String(number);
+  const len = str.length;
+  for (let i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 const input = prompt("Enter a palindrome number");
-alert(nextPalindrome(input));
+const parsedInput = parseInt(input);
+
+if (isNaN(parsedInput) || parsedInput <= 0) {
+  alert("Invalid input. Please enter a positive integer.");
+} else {
+  const result = nextPalindrome(parsedInput);
+  alert(`The next palindrome is: ${result}`);
+}
